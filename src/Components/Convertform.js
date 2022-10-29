fe99aaee60df9c11bb801b43c714b36929052a86
suichanwa <augustinnegura746@gmail.com>
2022-10-29 19:29:23 +0300

    unitconvertor

---
+++ src/Components/Convertform.js
@@ -0,0 +1,21 @@
+import React from "react";
+import { Form } from "react-final-form";
+
+const Convertform = () => {
+    return(
+        <Form onSubmit={() => {
+            console.log("tetisng");
+        }}>
+            {({ handleSubmit }) => (
+                <form onSubmit={ handeSubmit }>
+                    <input placeholder="first" type="text"></input>
+                    +
+                    <input placeholder="last" type="text"></input>
+                    <button type="submit">Submit</button>
+                </form>
+            )}
+        </Form>
+    )
+}
+
+export default Convertform;
