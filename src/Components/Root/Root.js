6f50487bd4e33cfc7a5a07e55e1bbdca87e50a65
suichanwa <augustinnegura746@gmail.com>
2022-11-01 10:46:16 +0300

    adding unit convert

--- src/Components/Root/Root.js
+++ src/Components/Root/Root.js
@@ -1,15 +1,23 @@
 import React from 'react';
 import { Form } from 'react-final-form';
+import configureMeasurements from 'convert-units';
+import allMeasures from 'convert-units';
 
 const Root = () => {
-    
+    const convert = configureMeasurements(allMeasures);
+
     return (
         <Form onSubmit={() => {
-            alert("testing");
-        }}>
+            console.log(a);
+
+        }}> 
             {props => (
                 <form onSubmit={props.handleSubmit}>
-
+                    <lable>
+                        <input type="text" placeholder='Km' />
+                        <input type="text" placeholder='metters' />
+                        <button name="text">name</button>
+                    </lable>   
                 </form>
             )}
         </Form>
